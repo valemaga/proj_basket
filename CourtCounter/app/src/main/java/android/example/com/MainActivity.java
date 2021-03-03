7115eacc8a4c8efebb948e532360d10d3f38a82d
Vasco Magalhães <vascolema@gmail.com>
2021-03-03 15:17:49 +0000

    Criação do projeto inicial

---
+++ CourtCounter/app/src/main/java/android/example/com/MainActivity.java
@@ -0,0 +1,13 @@
+package android.example.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
