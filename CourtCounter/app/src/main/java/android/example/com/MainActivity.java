ad9378b2e06a6cddb0055fb8828ebe20439aabd0
Vasco Magalhães <vascolema@gmail.com>
2021-03-03 16:07:30 +0000

    Lógica implementada para o score da Equipa A

--- CourtCounter/app/src/main/java/android/example/com/MainActivity.java
+++ CourtCounter/app/src/main/java/android/example/com/MainActivity.java
@@ -2,12 +2,40 @@ package android.example.com;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int firstTeamScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firstTeamScore = 0;
+    }
+
+    public void addToTeamA(View view){
+        int idButton = view.getId();
+
+        switch (idButton) {
+            case (R.id.plus_three):
+                firstTeamScore += 3;
+                break;
+            case (R.id.plus_two):
+                firstTeamScore += 2;
+                break;
+            case (R.id.plus_one):
+                firstTeamScore += 1;
+                break;
+        }
+        updateTeamAScore();
+    }
+
+    public void updateTeamAScore(){
+        TextView teamAScore = (TextView) findViewById(R.id.team_a_score);
+        teamAScore.setText(String.valueOf(firstTeamScore));
     }
 }
