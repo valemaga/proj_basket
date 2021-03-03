108fa74ef25cf6dfb4e1b5256471b4829feb2355
Vasco Magalhães <vascolema@gmail.com>
2021-03-03 16:26:47 +0000

    Acrescentada view e lógica para equipa B

--- CourtCounter/app/src/main/java/android/example/com/MainActivity.java
+++ CourtCounter/app/src/main/java/android/example/com/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     int firstTeamScore;
+    int secondTeamScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         firstTeamScore = 0;
+        secondTeamScore = 0;
     }
 
     public void addToTeamA(View view){
@@ -31,11 +33,33 @@ public class MainActivity extends AppCompatActivity {
                 firstTeamScore += 1;
                 break;
         }
-        updateTeamAScore();
+        updateTeamAScore(firstTeamScore);
     }
 
-    public void updateTeamAScore(){
+    public void addToTeamB(View view){
+        int idButton = view.getId();
+
+        switch (idButton) {
+            case (R.id.plus_three_b):
+                secondTeamScore += 3;
+                break;
+            case (R.id.plus_two_b):
+                secondTeamScore += 2;
+                break;
+            case (R.id.plus_one_b):
+                secondTeamScore += 1;
+                break;
+        }
+        updateTeamBScore(secondTeamScore);
+    }
+
+    public void updateTeamAScore(int score){
         TextView teamAScore = (TextView) findViewById(R.id.team_a_score);
-        teamAScore.setText(String.valueOf(firstTeamScore));
+        teamAScore.setText(String.valueOf(score));
+    }
+
+    public void updateTeamBScore(int score){
+        TextView teamBScore = (TextView) findViewById(R.id.team_b_score);
+        teamBScore.setText(String.valueOf(score));
     }
 }
